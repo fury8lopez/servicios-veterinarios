a560a90e498445d7eb1390c8a611155f4ccbd468
Usuario <juansim87@gmail.com>
2025-03-13 12:44:15 +0100

    first form

--- index.js
+++ index.js
@@ -49,17 +49,59 @@ let clientes = [
 
 // renderClients();
 
-const clientFormSection = document.getElementById("form");
+
+//SECCIÓN DE FORM
+const clientFormSection = document.getElementById("info-form");
+
+//CAJA DE FORM
+const clientFormBox = document.createElement("div");
+clientFormBox.classList.add("form-box");
+
+//TÍTULO FORM
+const formTitle = document.createElement("h2");
+formTitle.textContent = "Queremos conocerte";
+
+//FORM
 
 const clientForm = document.createElement("form");
-clientForm.classList.add("form-box");
+clientForm.classList.add("form");
+
+///ELEMENTOS FORM
+
+//NOMBRE CLIENTE
+const clientNameBox = document.createElement("div");
+clientNameBox.classList.add("form-item");
+
+const clientNameLabel = document.createElement("label");
+clientNameLabel.setAttribute("for", "client-name");
+clientNameLabel.textContent = "Nombre y apellidos: "
+
+const clientNameInput = document.createElement("input");
+clientNameInput.setAttribute("id", "client-name");
+clientNameInput.setAttribute("placeholder", "Ej: John Doe");
+
+clientNameBox.append(clientNameLabel, clientNameInput)
+
+//NOMBRE MASCOTA
+
+const petNameBox = document.createElement("div");
+petNameBox.classList.add("form-item");
+
+const petNameLabel = document.createElement("label");
+petNameLabel.setAttribute("for", "pet-name");
+petNameLabel.textContent = "Nombre de mascota: "
+
+const petNameInput = document.createElement("input");
+petNameInput.setAttribute("id", "pet-name");
+petNameInput.setAttribute("placeholder", "Ej: Micifús");
+
+petNameBox.append(petNameLabel, petNameInput)
 
 
 
-
-
-
-clientFormSection.appendChild(clientForm)
+clientForm.append(clientNameBox, petNameBox);
+clientFormBox.append(formTitle, clientForm);
+clientFormSection.appendChild(clientFormBox)
 
 
 
